769f0d1a67b9b7d14f610afc86eeb63c4f172fd8
Molly <26528097+mollimoll@users.noreply.github.com>
2021-08-03 21:13:05 +0100

    Extract DropTable to own file

---
+++ src/components/DropTable.tsx
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+import { ADD_AFTER_IMG, ADD_BEFORE_IMG } from '../utils/constants';
+import { ColSpacerMini, Row } from './Grid';
+
+const StyledCell = styled.div`
+  font-size: inherit;
+  font-family: inherit;
+
+  flex: 1;
+  width: 100%;
+
+  border: 0.1em solid #000000;
+  border-radius: 0.12em;
+`;
+
+const StyledImg = styled.img`
+  box-sizing: border-box;
+  max-width: 100%;
+  vertical-align: bottom;
+`;
+
+type CellProps = {
+  name: string;
+  action?: string;
+  dispatch?: React.Dispatch<any>;
+  children: JSX.Element[] | JSX.Element;
+};
+
+type Drag = React.DragEvent<HTMLDivElement>;
+
+const Cell = ({ name, action, dispatch, children }: CellProps) => {
+  const defaultDrag = (e: Drag) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleDrop = (e: Drag) => {
+    e.preventDefault();
+    dispatch &&
+      dispatch({
+        type: action,
+        name,
+        imgSrc: e.dataTransfer.getData('text/uri-list'),
+      });
+    e.stopPropagation();
+  };
+
+  return (
+    <StyledCell
+      onDrop={(e: Drag) => handleDrop(e)}
+      onDragOver={(e: Drag) => defaultDrag(e)}
+      onDragEnter={(e: Drag) => defaultDrag(e)}
+      onDragLeave={(e: Drag) => defaultDrag(e)}
+    >
+      {children}
+    </StyledCell>
+  );
+};
+export const DropTable = ({ data, dispatch }) => {
+  const headerRow = ['Device', 'Before', 'After'];
+
+  return (
+    <>
+      <Row>
+        {headerRow.map((name, i) => (
+          <>
+            {i !== 0 && <ColSpacerMini />}
+            <Cell name={name}>
+              <strong>{name}</strong>
+            </Cell>
+          </>
+        ))}
+      </Row>
+      {Object.keys(data).map((key) => {
+        const { beforeImg, afterImg } = data[key];
+        return (
+          <Row key={key}>
+            <Cell name={key}>
+              <p>{key}</p>
+            </Cell>
+            <ColSpacerMini />
+            <Cell name={key} dispatch={dispatch} action={ADD_BEFORE_IMG}>
+              <StyledImg key={beforeImg} src={beforeImg} id={beforeImg} />
+            </Cell>
+            <ColSpacerMini />
+            <Cell name={key} dispatch={dispatch} action={ADD_AFTER_IMG}>
+              <StyledImg key={afterImg} src={afterImg} id={afterImg} />
+            </Cell>
+          </Row>
+        );
+      })}
+    </>
+  );
+};
--- src/components/DropZone.tsx
+++ src/components/DropZone.tsx
@@ -1,68 +1,11 @@
 import { Dispatch, SetStateAction, useReducer } from 'react';
-import styled from 'styled-components';
+import { ADD_AFTER_IMG, ADD_BEFORE_IMG } from '../utils/constants';
 import { ImageData } from '../utils/parsing';
 import { createMarkdownTable } from '../utils/table';
 import { Button } from './Button';
-import { Col, Row, ColSpacer, ColSpacerMini } from './Grid';
+import { DropTable } from './DropTable';
+import { Col, ColSpacer, Row } from './Grid';
 import { Images } from './Images';
-
-const ADD_BEFORE_IMG = 'ADD_BEFORE_IMG';
-const ADD_AFTER_IMG = 'ADD_AFTER_IMG';
-
-const StyledCell = styled.div`
-  font-size: inherit;
-  font-family: inherit;
-
-  flex: 1;
-  width: 100%;
-
-  border: 0.1em solid #000000;
-  border-radius: 0.12em;
-`;
-
-const StyledImg = styled.img`
-  box-sizing: border-box;
-  max-width: 100%;
-  vertical-align: bottom;
-`;
-
-type CellProps = {
-  name: string;
-  action?: string;
-  dispatch?: React.Dispatch<any>;
-  children: JSX.Element[] | JSX.Element;
-};
-
-type Drag = React.DragEvent<HTMLDivElement>;
-
-const Cell = ({ name, action, dispatch, children }: CellProps) => {
-  const defaultDrag = (e: Drag) => {
-    e.preventDefault();
-    e.stopPropagation();
-  };
-
-  const handleDrop = (e: Drag) => {
-    e.preventDefault();
-    dispatch &&
-      dispatch({
-        type: action,
-        name,
-        imgSrc: e.dataTransfer.getData('text/uri-list'),
-      });
-    e.stopPropagation();
-  };
-
-  return (
-    <StyledCell
-      onDrop={(e: Drag) => handleDrop(e)}
-      onDragOver={(e: Drag) => defaultDrag(e)}
-      onDragEnter={(e: Drag) => defaultDrag(e)}
-      onDragLeave={(e: Drag) => defaultDrag(e)}
-    >
-      {children}
-    </StyledCell>
-  );
-};
 
 const dummyCellData = {
   Mobile: { beforeImg: '', afterImg: '' },
@@ -113,43 +56,6 @@ export const DropZone = ({ onButtonClick, images }: Props) => {
           <DropTable data={data} dispatch={dispatch} />
         </Col>
       </Row>
-    </>
-  );
-};
-
-export const DropTable = ({ data, dispatch }) => {
-  const headerRow = ['Device', 'Before', 'After'];
-
-  return (
-    <>
-      <Row>
-        {headerRow.map((name, i) => (
-          <>
-            {i !== 0 && <ColSpacerMini />}
-            <Cell name={name}>
-              <strong>{name}</strong>
-            </Cell>
-          </>
-        ))}
-      </Row>
-      {Object.keys(data).map((key) => {
-        const { beforeImg, afterImg } = data[key];
-        return (
-          <Row key={key}>
-            <Cell name={key}>
-              <p>{key}</p>
-            </Cell>
-            <ColSpacerMini />
-            <Cell name={key} dispatch={dispatch} action={ADD_BEFORE_IMG}>
-              <StyledImg key={beforeImg} src={beforeImg} id={beforeImg} />
-            </Cell>
-            <ColSpacerMini />
-            <Cell name={key} dispatch={dispatch} action={ADD_AFTER_IMG}>
-              <StyledImg key={afterImg} src={afterImg} id={afterImg} />
-            </Cell>
-          </Row>
-        );
-      })}
     </>
   );
 };
