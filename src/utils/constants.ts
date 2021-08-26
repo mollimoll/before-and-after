ca6ea7ffa619dc53d804e0a26f2f5d987edc7634
Molly <mollimoll@users.noreply.github.com>
2021-08-26 21:29:07 +0100

    Updates

--- src/utils/constants.ts
+++
@@ -1,7 +0,0 @@
-export const ADD_BEFORE_IMG = 'ADD_BEFORE_IMG';
-export const ADD_AFTER_IMG = 'ADD_AFTER_IMG';
-export const RESET = 'RESET';
-export const EDIT = 'EDIT';
-export const EDIT_ENDED = 'EDIT_ENDED';
-export const ADD_ROW = 'ADD_ROW';
-export const DELETE_ROW = 'DELETE_ROW';
